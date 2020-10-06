1076d2e1d65325e6aec16981e06443409657c2a2
Joe Pearson <joe.c.pearson95@gmail.com>
2020-10-06 13:46:37 +0100

    first upload
    simple tutorial following to get an understanding of detectron2

---
+++ test.py
@@ -0,0 +1,28 @@
+# Some basic setup:
+# Detectron2 utils
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import cv2
+
+# Image obtained
+im = cv2.imread("input.jpg")
+#cv2.imshow('ImageWindow',im)
+
+# Create config
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml")
+
+# Predictor created
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imshow('', out.get_image()[:, :, ::-1])
+cv2.waitKey(0)
